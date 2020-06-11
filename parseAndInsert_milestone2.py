bd959a89a2403466e5f1c1b1d53d64824f87642d
Admin <admin@Admins-iMac.local>
2020-06-10 21:53:45 -0700

    Adds 'insert2UserTable' code (needs verification.

--- parseAndInsert_milestone2.py
+++ parseAndInsert_milestone2.py
@@ -14,14 +14,13 @@ def int2BoolStr (value):
 def insert2BusinessTable():
     #reading the JSON file
     with open('./yelp_business.JSON','r') as f:    #TODO: update path for the input file
-        #outfile =  open('./yelp_business.SQL', 'w')  #uncomment this line if you are writing the INSERT statements to an output file.
         line = f.readline()
         count_line = 0
 
         #connect to yelpdb database on postgres server using psycopg2
         #TODO: update the database name, username, and password
         try:
-            conn = psycopg2.connect("dbname='yelpdb' user='postgres' host='localhost' password='none'")
+            conn = psycopg2.connect("dbname='yelpdb' user='postgres' host='localhost' password='mustafa'")
         except Exception as error:
             print('ERROR: Unable to connect to the database!')
             print('Error message:', error)
@@ -30,23 +29,19 @@ def insert2BusinessTable():
         while line:
             data = json.loads(line)
             # Generate the INSERT statement for the current business
-            # TODO: The below INSERT statement is based on a simple (and incomplete) businesstable schema. Update the statement based on your own table schema and
-            # include values for all businessTable attributes
             sql_str = "INSERT INTO Business (businessID, businessName, address, avgScore, city, detailedInfo, numCheckins, numReviews, businessState, stars, openStatus, zip) " \
                       "VALUES ('" + clearnStr4SQL(data['businessID']) + "','" + clearnStr4SQL(data['businessName']) + "','" + clearnStr4SQL(data['address']) + "','" \
                       str(data['avgScore']) + "','" + cleanStr4SQL(data['city']) + "','" + cleanStr4SQL(data['detailedInfo']) + "','" + str(data['numCheckins'])  \
                       + "','" + str(data['numReviews']) + "','" + cleanStr4SQL(data['businessState']) + "','" + str(data['stars']) + "','" + str(data['openStatus']) |
                       + "','" + str(data['zip']) + ");"
-                      
+
             try:
                 cur.execute(sql_str)
             except Exception as error:
-                print('ERROR: Insert to business table failed!')
+                print('ERROR: Insert to Business table failed!')
                 print('Error message:', error)
 
             conn.commit()
-            # optionally you might write the INSERT statement to a file.
-            # outfile.write(sql_str)
 
             line = f.readline()
             count_line +=1
@@ -55,7 +50,46 @@ def insert2BusinessTable():
         conn.close()
 
     print(count_line)
-    #outfile.close()  #uncomment this line if you are writing the INSERT statements to an output file.
+    f.close()
+
+def insert2UserTable():
+    with open('./yelp_user.JSON','r') as f:    #TODO: update path for the input file
+        line = f.readline()
+        count_line = 0
+
+        #connect to yelpdb database on postgres server using psycopg2
+        #TODO: update the database name, username, and password
+        try:
+            conn = psycopg2.connect("dbname='yelpdb' user='postgres' host='localhost' password='mustafa'")
+        except Exception as error:
+            print('ERROR: Unable to connect to the database!')
+            print('Error message:', error)
+        cur = conn.cursor()
+
+        while line:
+            data = json.loads(line)
+            # Generate the INSERT statement for the current user
+            sql_str = "INSERT INTO UserTable (userID, firstName, lastName, avgStars, dateJoined, latitude, longitude, info, isFanOf, isFriendsWith, numFans, votes, favorites) " \
+                      "VALUES ('" + clearnStr4SQL(data['userID']) + "','" + clearnStr4SQL(data['firstName']) + "','" + clearnStr4SQL(data['lastName']) + "','" + \
+                      str(data['avgStars']) + "','" +  clearnStr4SQL(data['dateJoined']) + "','" + str(data['latitude']) + "','" + str(data['longitude']) + "','" +  \
+                      clearnStr4SQL(data['info']) + "','" + clearnStr4SQL(data['isFanOf']) + "','" + clearnStr4SQL(data['isFriendsWith']) + "','" + \
+                      str(data['numFans']) + "','" + str(data['votes']) + "','" + clearnStr4SQL(data['favorites'])");"
+
+            try:
+                cur.execute(sql_str)
+            except Exception as error:
+                print('ERROR: Insert to UserTable failed!')
+                print('Error message:', error)
+
+            conn.commit()
+
+            line = f.readline()
+            count_line +=1
+
+        cur.close()
+        conn.close()
+
+    print(count_line)
     f.close()
 
 
