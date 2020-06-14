3b01b727f2a897942c7b910011575154810fd9e6
Jack <temp@gmail.com>
2020-06-14 09:13:33 -0700

    Adding update statements

---
+++ M2/TeamJJJ_Update.sql
@@ -0,0 +1,8 @@
+UPDATE business
+SET numcheckins = (SELECT count(checkinbusinessid) from checkin WHERE business.businessid = checkin.checkinbusinessid)
+
+UPDATE business
+SET reviewcount = (SELECT count(businessid) from review WHERE business.businessid = review.businessid)
+
+UPDATE business
+SET reviewrating = (SELECT AVG(stars) from review WHERE business.businessid = review.businessid)
